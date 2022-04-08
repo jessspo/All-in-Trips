f0599f0b589ecc7f934da3134a746ff9fa9bf0f1
Arne Rief <arne-rief_student2022@wilder.school>
2022-04-08 12:31:19 +0200

    First commit for testing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  // states for our data:
+  let [cityData, setCityData] = useState({});
+  let [weatherData, setWeatherData] = useState({});
+  let [visaData, setVisaData] = useState({});
+
+  // Function to show stuff when button clicked:
+  let [apiLoaded, setApiLoaded] = useState(false);
+  
+  // Function to get geocoordinates for city name on click:
+  const getCityCoordinates = function() {
+    fetch("http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=fab801f7e2e8bfaec7313b7ef6c6719a")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      setCityData(data);
+      setApiLoaded(true);
+    });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Sandbox API Test</h1>
+      <h3>Click this button to get city geocoordinates</h3>
+      <button onClick={getCityCoordinates}>Go!</button>
+      {apiLoaded === true 
+        ? <p>{cityData[0].name}</p> 
+        : <p>Nothing to see yet.</p>
+      }
     </div>
   );
 }
