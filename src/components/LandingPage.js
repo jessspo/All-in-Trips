ebd2c1a67251afeda41d0b4998f24f3215e06ea3
Arne Rief <arne-rief_student2022@wilder.school>
2022-05-10 14:40:25 +0200

    changed  welcome message in landing page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -7,7 +7,7 @@ const LandingPage = () => {
     <header className='landing-container'>
       <img className="img-landing" src='/images/img-landing.png' alt="picture of a travel map" />
       <h1>TRAVEL MODE</h1>
-      <p className='subtext'>look for flights, hotels and weather below</p>
+      <p className='subtext'>fill in the form below to find the best flights, hotels and weather</p>
     </header>
   );
 }
